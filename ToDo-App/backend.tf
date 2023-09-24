519e69eefaf64ab5e469856074a1484de66ff1b8
vinayreddy05 <kvinayreddy11@gmail.com>
2023-09-24 14:43:18 -0500

    updated bucket name

--- ToDo-App/backend.tf
+++ ToDo-App/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "10weeksofcloudops-week4"
+    bucket = "10weeksofcloudops-week3"
     key    = "backend/ToDo-App.tfstate"
     region = "us-east-1"
     dynamodb_table = "10weeksofcloudops-dynamo"
